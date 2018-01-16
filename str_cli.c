d0161ff6bd7ad163ab167b0393671f5cdc809cd7
qiuyu <956318206@qq.com>
2018-01-16 09:37:08 +0800

    use select to translate blocking I/O to I/O multiplexing

--- str_cli.c
+++ str_cli.c
@@ -3,24 +3,47 @@
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
+#include <sys/select.h>
+#include <sys/time.h>
 
 void str_cli(FILE *fp, int sockfd)
 {
+	int maxfdp1, res;
+	fd_set rset;
 	ssize_t n;
 	char sendline[LINE_MAX], recvline[LINE_MAX];
 
-	while ( fgets(sendline, sizeof(sendline), fp) != NULL) {
-		if (writen(sockfd, sendline, strlen(sendline)) == -1) {
-			perror("writen");
+	FD_ZERO(&rset);
+
+	while (1) {
+		FD_SET(fileno(fp), &rset);
+		FD_SET(sockfd, &rset);
+		if (fileno(fp) < sockfd)
+			maxfdp1 = sockfd + 1;
+		else
+			maxfdp1 = fileno(fp) + 1;
+
+		if ( (res = select(maxfdp1, &rset, NULL, NULL, NULL)) == -1) {
+			perror("select");
 			exit(EXIT_FAILURE);
 		}
-		if ( (n = readline(sockfd, recvline, sizeof(recvline)) ) == -1) {
-			perror("readline");
-			exit(EXIT_FAILURE);
-		} else if (n == 0) {
-			fprintf(stdout, "str_cli: server terminated permaturely\n");
-			exit(EXIT_FAILURE);
+		if (FD_ISSET(sockfd, &rset)) {	/* socket is readable */
+			if ( (n = readline(sockfd, recvline, sizeof(recvline))) == -1) {
+				perror("readline");
+				exit(EXIT_FAILURE);
+			} else if (n == 0) {
+				fprintf(stderr, "str_cli: server terminated prematurely\n");
+				exit(EXIT_FAILURE);
+			}
+			fputs(recvline, stdout);
 		}
-		fputs(recvline, stdout);
+		if (FD_ISSET(fileno(fp), &rset)) {	/* input is readable */
+			if (fgets(sendline, sizeof(sendline), fp) == NULL)
+				return ;	/* all done */
+			if (writen(sockfd, sendline, strlen(sendline)) == -1) {
+				perror("writen");
+				exit(EXIT_FAILURE);
+			}
+		}
 	}
 }
